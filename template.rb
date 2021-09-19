7ff02aab11985efd8e22d0e9ce827e94c5017660
ervinismu <social.ervin@gmail.com>
2021-09-19 16:49:56 +0700

    update template

--- template.rb
+++ template.rb
@@ -2,54 +2,62 @@
 ### ervinismu | ervinismu.com
 ###
 
+# rvm environment related
+# copy_file '.ruby-gemset'
+# gsub_file '.ruby-gemset', /#appname/, "#{@app_name}"
+# copy_file '.ruby-version'
+
 def source_paths
   [File.expand_path(File.dirname(__FILE__))]
 end
 
-# Setup model for Auth
-generate(:model, 'user username:string email:string password_digest:string recovery_password_digest:string')
-insert_into_file 'app/models/user.rb', after: "class User < ApplicationRecord\n" do <<-EOF
-  # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
-  has_secure_password
-  has_secure_password :recovery_password, validations: false
+if yes?('Would you like to setup User model for Auth? (y/n)')
+  # Setup model for Auth
+  generate(:model, 'user username:string email:string password_digest:string recovery_password_digest:string')
+  insert_into_file 'app/models/user.rb', after: "class User < ApplicationRecord\n" do <<-EOF
+    # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
 
-  # validations
-  validates :email, uniqueness: true
-EOF
-end
+    # validations
+    validates :email, uniqueness: true
+  EOF
+  end
 
-# setup gem
-gem 'figaro' # environemnts
-gem 'jwt' # authentication
-gem 'bcrypt', '~> 3.1.7' # # Use Active Model has_secure_password
+  # setup gem
+  gem 'figaro' # environemnts
+  gem 'jwt' # authentication
+  gem 'bcrypt', '~> 3.1.7' # Use Active Model has_secure_password
 
-# add config file
-file 'config/application.yml', <<-CODE
-AUTH_SECRET: 'yoursecrethere'
-AUTH_EXP_DAY: '1'
-CODE
+  # add config file
+  file 'config/application.yml', <<-CODE
+  AUTH_SECRET: 'yoursecrethere'
+  AUTH_EXP_DAY: '1'
+  CODE
 
-# remove and copy controllers
-run 'rm -rf app/controllers'
-directory 'app/controllers', 'app/controllers'
+  # remove and copy controllers
+  run 'rm -rf app/controllers'
+  directory 'app/controllers', 'app/controllers'
 
-# copy lib
-directory 'lib', 'lib'
+  # copy lib
+  directory 'lib', 'lib'
 
-# configure routing
-insert_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do <<-EOF
-  scope :api do
-    scope :core do
-      scope :users do
-        post '/sign-in', to: 'users#sign_in'
-        post '/sign-up', to: 'users#sign_up'
-        get '/all', to: 'users#index'
-        get '/me', to: 'users#profile'
+  # configure routing
+  insert_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do <<-EOF
+    scope :api do
+      scope :core do
+        scope :users do
+          post '/sign-in', to: 'users#sign_in'
+          post '/sign-up', to: 'users#sign_up'
+          get '/all', to: 'users#index'
+          get '/me', to: 'users#profile'
+        end
       end
     end
+  EOF
   end
-EOF
 end
+
 
 # load lib
 insert_into_file 'config/application.rb', after: "class Application < Rails::Application\n" do <<-EOF
@@ -94,3 +102,11 @@ if yes?('Would you like to setup Heroku? (y/n)')
   copy_file 'deploy_heroku.sh', 'deploy_heroku.sh'
   run 'chmod +x deploy_heroku.sh'
 end
+
+# setup admin page
+# gem 'rails_admin', '~> 2.0'
+
+# # bundle
+run 'bundle install'
+
+# run 'rails g rails_admin:install'
